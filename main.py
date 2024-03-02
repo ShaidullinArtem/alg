5c280b01516daa015821a099579215fc3563c00f
unknown <rikschapov@gmail.com>
2024-03-02 17:13:55 +0500

    sinii traktor edet k nam u nego v prihepe kto-to pesenku poet

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+def is_prime(intt: int) -> bool:
+	for i in range(2, intt):
+		if intt//i == intt/i:
+			return False
+	else:
+		return True
 
 
 def main():
-	print('hello, Oleg')
+	print(is_prime(10))
 
 
 if __name__ == '__main__':
