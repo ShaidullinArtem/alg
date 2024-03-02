0336df991357c13d5373aeb436729f550417a10b
Zero_i00 <mrshelkinpro228@gmail.com>
2024-03-02 17:15:16 +0500

    create function number_sum

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 
 
+def number_sum(num: int) -> int:
+	
+	sum = 0
+	for i in str(num):
+		sum += int(i)
+	
+	return sum
+
 def main():
-	print('hello, Oleg')
+	print(number_sum(123))
 
 
 if __name__ == '__main__':
